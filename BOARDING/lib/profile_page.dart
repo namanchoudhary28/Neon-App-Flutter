7129d62683b65f3a562fc5d7363d4abe3292cadc
udbhav100 <i.udbhav@gmail.com>
2020-05-20 02:50:14 +0530

    Added Acheivements

--- BOARDING/lib/profile_page.dart
+++ BOARDING/lib/profile_page.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:link/link.dart';
 // import 'package:BOARDING/login_page.dart';
 
 class HomePage extends StatelessWidget {
   static String tag = 'home-page';
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -54,13 +53,21 @@ class HomePage extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Image.network(
-                'https://img.icons8.com/android/24/000000/phone.png',
-                scale: 1.1,
+              Link(
+                child: Image.network(
+                  'https://img.icons8.com/android/24/000000/phone.png',
+                  scale: 1.1,
+                ),
+                url: 'https://www.google.com',
+                onError: _showErrorSnackBar,
               ),
-              Image.network(
-                'https://img.icons8.com/color/48/000000/facebook-new.png',
-                scale: 1.2,
+              Link(
+                child: Image.network(
+                  'https://img.icons8.com/color/48/000000/facebook-new.png',
+                  scale: 1.2,
+                ),
+                url: 'https://www.facebook.com',
+                onError: _showErrorSnackBar,
               ),
               Image.network(
                 'https://img.icons8.com/color/48/000000/twitter.png',
@@ -109,15 +116,27 @@ class HomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'HOBBIES AND INTERESTS',
-              style: TextStyle(
-                fontSize: 17.0,
-                letterSpacing: 1.7,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'HOBBIES AND INTERESTS',
+                  style: TextStyle(
+                    fontSize: 17.0,
+                    letterSpacing: 1.7,
+                  ),
+                ),
               ),
-            ),
+                  //padding: const EdgeInsets.all(8.0),
+              Link(
+                  child: Image.network('https://img.icons8.com/windows/32/000000/edit.png'),
+              url:'Edit URL',
+                onError: _showErrorSnackBar,),
+
+
+            ],
           ),
         ],
       ),
@@ -255,8 +274,58 @@ class HomePage extends StatelessWidget {
           MyProjects('Booking 3', '1 May 2021'),
         ],
       ),
-    ),],)
-    ;   // MyProjects function is defined at the end. Usage Syntax: MyProjects(heading,subheading)
+    ),],); // MyProjects function is defined at the end. Usage Syntax: MyProjects(heading,subheading)
+
+    final badges = Container(
+      height: 170,
+      color: Colors.cyan[100],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'BADGES',
+              style: TextStyle(
+                fontSize: 17.0,
+                letterSpacing: 1.7,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    final achievemnets=Column(
+      children: <Widget>[
+        Row(
+        children:<Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+          child: Text(
+            'ACHIEVEMENTS',
+            style: TextStyle(fontSize: 17,letterSpacing: 1.7,),
+          )
+        ),
+          Link(
+            child: Image.network('https://img.icons8.com/windows/32/000000/edit.png'),
+            url:'Edit URL',
+            onError: _showErrorSnackBar,),
+      ],
+    ),
+        Container(
+            width:(MediaQuery.of(context).size.width),
+            padding: EdgeInsets.fromLTRB(16, 5, 16, 5),
+              child:Column(
+                children:<Widget>[
+              _MyAchievemnts('May 10','Testing','Testin text'),
+              _MyAchievemnts('May 11','Testing2','Grammaticality Wise men speak because they have something to say; Fools because they have to say something'),
+
+    ],
+    ),),
+    ],
+    );
 
     final body = ListView(
       scrollDirection: Axis.vertical,
@@ -284,6 +353,11 @@ class HomePage extends StatelessWidget {
         ),
         skills,
         projects,
+        SizedBox(
+          height: 30,
+        ),
+        badges,
+        achievemnets,
       ],
     );
 
@@ -314,5 +388,44 @@ class HomePage extends StatelessWidget {
               ),
             ),
     );
+  }
+
+  Row _MyAchievemnts(String date, String heading,String subHeading){
+    return
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            date,
+            style: TextStyle(fontSize: 17),
+          ),
+     Container(
+       height: 100,
+       width:300,
+     child:Card(
+        shape:RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        color: Colors.redAccent,
+        child:Wrap(
+          children: <Widget>[
+            ListTile(
+              title:Text(heading,style: TextStyle(color: Colors.black)),
+              subtitle: Text(subHeading,style: TextStyle(color: Colors.black)),
+            ),
+          ],
+        ),
+      ),),
+        ],
+      );
+  }
+  void _showErrorSnackBar() {
+
+      Scaffold(
+        body: SnackBar(
+          content: Text('Oops... the URL couldn\'t be opened!'),
+        ),
+      )
+    ;
   }
 }
