772050f88512a49b820105a69c59a4fd7980c30a
namanchoudhary28 <65090808+namanchoudhary28@users.noreply.github.com>
2020-05-25 14:37:10 +0530

    COMPLETED CAHNGES
    replace this via old lib/edit_info.dart

--- edit_info.dart
+++ edit_info.dart
@@ -12,69 +12,296 @@ class EDITINFO extends StatefulWidget {
 }
 
 class _EDITINFOState extends State<EDITINFO> {
-   var _name;
-   var _location;
-   var _about;
-   final locationcon= new TextEditingController();
+   var _name;          //accesing name 
+   var _location;     //accesing location
+   var _about;        //acessing about me
+   var _phoneno;      //acessing phone no
+   var _whatsappno;  //acessing whatsappno
+   var _skypeno;      //acessing skype no
+   var _linkedin;   //acessing linkedin
+   var _twitter;    //acessing twitter
+   var _facebook;   //acessing facebook
+  final namecon= new TextEditingController();
+
+  final locationcon= new TextEditingController();
   final aboutcon= new TextEditingController();
+  final phonecon= new TextEditingController();
+  final whatsappnocon= new TextEditingController();
+  final skypenocon= new TextEditingController();
+  final linkedincon= new TextEditingController();
+  final twittercon= new TextEditingController();
+  final facebookcon= new TextEditingController();
+
 
     
-  final namecon= new TextEditingController();
   @override
   Widget build(BuildContext context) {
 
-    // final name= TextFormField(
-    //   // keyboardType: TextInputType.emailAddress,
-    //   validator: (value) {
-    //     if (value.isEmpty) {
-    //       return 'Please enter your email address';
-    //     }
-    //     return null;
-    //   },
-    //   autofocus: false,
-    //   decoration: InputDecoration(
-    //       hintText: 'Name',
-    //       contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0)
-    //   ),
-    // );
-    // var _name;
-    
-    // final namecon= new TextEditingController();
-    
-    
-    final name =TextFormField(
-      controller: namecon,
-      decoration: InputDecoration(labelText: 'Name',
-          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0)
-      ),
-      // maxLength: 10,
-      // validator: (String value) {
-      //   if (value.isEmpty) {
-      //     return 'Name is Required';
-      //   }
+        final basicname = Padding(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.le,
+        children: <Widget>[
+          InkWell(
+            child: Image.network('https://img.icons8.com/metro/24/000000/info.png'),
+            onTap: (){
+              //      Navigator.push(context,MaterialPageRoute(builder: (context){
 
-      //   return null;
-      // },
-      // onSaved: (String value) {
-      //   _name = value;
-      // },
+              // return new MyApp();
+
+
+              // }));
+
+
+            },
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            'BASIC',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              
+              
+            ),
+          ),
+
+        ],
+      ),
     );
-    final location =TextFormField(
-      controller: locationcon,
-      decoration: InputDecoration(labelText: 'Location',
-          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0)
-      ),
-      // maxLength: 10,
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'Location is Required';
-        }
+    
+    
+     final name = Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+      child: TextFormField(
+        controller: namecon,
+        decoration: InputDecoration(labelText: 'Name',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0)
+        ),
+        // maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Name is Required';
+          }
 
-        return null;
-      },
-      // onSaved: (String value) {
-      //   _name = value;
-      // },
+          return null;
+        },
+        // onSaved: (String value) {
+        //   _name = value;
+        // },
+      ),
+    );
+     final location = Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+      child: TextFormField(
+        controller: locationcon,
+        decoration: InputDecoration(labelText: 'Location',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0)
+        ),
+        // maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Location is Required';
+          }
+
+          return null;
+        },
+        // onSaved: (String value) {
+        //   _name = value;
+        // },
+      ),
+    );
+     final communicationname = Padding(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.le,
+        children: <Widget>[
+          InkWell(
+            child: Image.network('https://img.icons8.com/material/24/000000/phone--v1.png'),
+            onTap: (){
+              //      Navigator.push(context,MaterialPageRoute(builder: (context){
+
+              // return new MyApp();
+
+
+              // }));
+
+
+            },
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            'COMMUNICATION',
+            style: TextStyle(
+             fontWeight: FontWeight.bold,
+              fontSize: 18,
+              
+            ),
+          ),
+
+        ],
+      ),
+    );
+    final phone =Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+      child: TextFormField(
+        controller: phonecon,
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(labelText: 'Phone No',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0)
+        ),
+        // maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Phone no is Required';
+          }
+
+          return null;
+        },
+        // onSaved: (String value) {
+        //   _name = value;
+        // },
+      ),
+    );
+    final whatsapp =Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+      child: TextFormField(
+        controller: whatsappnocon,
+        keyboardType: TextInputType.number,
+
+        decoration: InputDecoration(labelText: 'Whatsapp',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0)
+        ),
+        // maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'whatsapp no is Required';
+          }
+
+          return null;
+        },
+        // onSaved: (String value) {
+        //   _name = value;
+        // },
+      ),
+    );
+    final skype =Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+      child: TextFormField(
+        controller: skypenocon,
+        decoration: InputDecoration(labelText: 'Skype',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0)
+        ),
+        // maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'skype id is Required';
+          }
+
+          return null;
+        },
+        // onSaved: (String value) {
+        //   _name = value;
+        // },
+      ),
+    );
+   
+    final linkedin =Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+      child: TextFormField(
+        controller: linkedincon,
+        decoration: InputDecoration(labelText: 'Linkedin',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0)
+        ),
+        // maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'linkdin id is Required';
+          }
+
+          return null;
+        },
+        // onSaved: (String value) {
+        //   _name = value;
+        // },
+      ),
+    );
+    final twitter =Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+      child: TextFormField(
+        controller: twittercon,
+        decoration: InputDecoration(labelText: 'Twitter',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0)
+        ),
+        // maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'twitter link is Required';
+          }
+
+          return null;
+        },
+        // onSaved: (String value) {
+        //   _name = value;
+        // },
+      ),
+    );
+    final facebook =Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+      child: TextFormField(
+        controller: facebookcon,
+        decoration: InputDecoration(labelText: 'Facebook',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0)
+        ),
+        // maxLength: 10,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'facebook id is Required';
+          }
+
+          return null;
+        },
+        // onSaved: (String value) {
+        //   _name = value;
+        // },
+      ),
+    );
+    final aboutname = Padding(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.le,
+        children: <Widget>[
+          InkWell(
+            child: Image.network('https://img.icons8.com/android/24/000000/pencil.png'),
+            onTap: (){
+              //      Navigator.push(context,MaterialPageRoute(builder: (context){
+
+              // return new MyApp();
+
+
+              // }));
+
+
+            },
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            'ABOUT ME',
+            style: TextStyle(
+               fontWeight: FontWeight.bold,
+              fontSize: 18,
+              
+            ),
+          ),
+
+        ],
+      ),
     );
     final aboutme= TextField(
       controller: aboutcon,
@@ -88,6 +315,26 @@ class _EDITINFOState extends State<EDITINFO> {
       maxLines: 6,
 
 
+    );Padding(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+      child: TextFormField(
+        controller: facebookcon,
+        decoration: InputDecoration(labelText: 'write here',
+            contentPadding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0)
+        ),
+        // maxLength: 10,
+        maxLines: 5,
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Name is Required';
+          }
+
+          return null;
+        },
+        // onSaved: (String value) {
+        //   _name = value;
+        // },
+      ),
     );
 
     var checkBoxvalue=false;
@@ -103,21 +350,9 @@ class _EDITINFOState extends State<EDITINFO> {
             
             scrollDirection: Axis.vertical,
             children: <Widget>[
+              basicname,
               
-              Container(
-                child: Align(
-                  heightFactor: 4,
-                  alignment: Alignment(-0.9, 0),
-                  child: Text("Basic",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-    
-                    ),
-    
-                  ),
-                ),
-              ),
+
               name,
               SizedBox(
                 height: 20,
@@ -126,138 +361,19 @@ class _EDITINFOState extends State<EDITINFO> {
               SizedBox(
                 height : 20,
               ),
-              Container(
-                child: Align(
-                  heightFactor: 2,
-                  alignment: Alignment(-0.9, 0),
-                  child: Text("COMMUNICATION",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-    
-                    ),
-    
-                  ),
-    
-    
-    
-    
-                ),
+              communicationname,
+      
+              phone,
+              whatsapp,
+              skype,
+              linkedin,
+              twitter,
+              facebook,
+               SizedBox(
+                height : 20,
               ),
-              Row(
-                  children: <Widget>[
-                    Checkbox(value: true,
-                        onChanged: (bool value){
-    
-    
-                        }
-                    ),
-                    Text("nnn"),
-    
-    
-    
-    
-                  ]
-    
-              ),
-              Row(
-                  children: <Widget>[
-                    Checkbox(value: true,
-                        onChanged: (bool value){
-    
-    
-                        }
-                    ),
-                    Text("nnn"),
-    
-    
-    
-    
-                  ]
-    
-              ),
-              Row(
-                  children: <Widget>[
-                    Checkbox(value: true,
-                        onChanged: (bool value){
-    
-    
-                        }
-                    ),
-                    Text("nnn"),
-    
-    
-    
-    
-                  ]
-    
-              ),
-              Row(
-                  children: <Widget>[
-                    Checkbox(value: true,
-                        onChanged: (bool value){
-    
-    
-                        }
-                    ),
-                    Text("nnn"),
-    
-    
-    
-    
-                  ]
-    
-              ),
-              Row(
-                  children: <Widget>[
-                    Checkbox(value: true,
-                        onChanged: (bool value){
-    
-    
-                        }
-                    ),
-                    Text("nnn"),
-    
-    
-    
-    
-                  ]
-    
-              ),
-              Row(
-                  children: <Widget>[
-                    Checkbox(value: true,
-                        onChanged: (bool value){
-    
-    
-                        }
-                    ),
-                    Text("nnn"),
-    
-    
-    
-    
-                  ]
-    
-              ),
-              Container(
-                child: Align(
-                  heightFactor: 2,
-                  alignment: Alignment(-0.9, 0),
-                  child: Text("About Me",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-    
-                    ),
-    
-                  ),
-    
-    
-    
-    
-                ),
-              ),
+            
+              aboutname,
               aboutme,
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -315,7 +431,14 @@ class _EDITINFOState extends State<EDITINFO> {
                         setState(() {
                           _name=namecon.text;
                           _location=locationcon.text;
-                          _about=aboutcon;
+                          _about=aboutcon.text;
+                          _phoneno=phonecon.text;
+                          _whatsappno=whatsappnocon.text;
+                          _skypeno=skypenocon.text;
+                          _linkedin=linkedincon.text;
+                          _twitter=twittercon.text;
+                          _facebook=facebookcon.text;
+                          
                         });
                         
                        },
@@ -331,12 +454,16 @@ class _EDITINFOState extends State<EDITINFO> {
             ],
           ),
           
-          Container(
-            height: 50,
-            child: Text("your name is $_name and your location is $_location  "),
+          // Container(
+          //   height: 500,
+          //   width: 100,
+          //   color: Colors.blue,
+          //   child: Text("your name is $_phoneno and your location is $_location  ,$_phoneno,$_about, $_whatsappno,$_skypeno,$_linkedin,$_twitter,$_facebook"),
+            
 
-          ),
-
+          // ),
+          
+     
 
 
 
