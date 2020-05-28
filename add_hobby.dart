70e6b82f240544e6df00814e2e61103f0ae61061
namanchoudhary28 <65090808+namanchoudhary28@users.noreply.github.com>
2020-05-28 05:45:02 +0530

    ADD hobby form
    to work u need to do underlined changes:
    1.in pubsec under assets you have to type - (-assets/banks/)
    2.add banks folder inside assets
    3. in profile page.dart type import ..../BOARDING/add_hobby.dart
    4. in hobby section inside inkwell navigator.push this dart file function

---
+++ add_hobby.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _selected;
+  String _selected2;
+  var image;      //accesing image folder location
+  var name;       //accessing name of hobby
+
+
+  List<Map> _myJson = [
+    {"id": '1', "image": "assets/banks/Dancing.png", "name": "Dancing"},
+    {"id": '2', "image": "assets/banks/Singing.png", "name": "Singing"},
+    {"id": '3', "image": "assets/banks/Cycling.png", "name": "Cycling"},
+    {"id": '4', "image": "assets/banks/Drawing.png", "name": "Drawing"},
+    {
+      "id": '5',
+      "image": "assets/banks/Cooking.png",
+      "name": "Cooking"
+    },
+    {"id": '6', "image": "assets/banks/Surfing.png", "name": "Surfing"},
+    {
+      "id": '7',
+      "image": "assets/banks/Writing.png",
+      "name": "Writing"
+    },
+    {"id": '8', "image": "assets/banks/Reading.png", "name": "Reading"},
+    {"id": '9', "image": "assets/banks/Archery.png", "name": "Archery"},
+    {
+      "id": '10',
+      "image": "assets/banks/Camping.png",
+      "name": "Camping"
+    },
+    // {"id": '11', "image": "assets/banks/rhb-now.png", "name": "RHB NOW"},
+    // {
+    //   "id": '12',
+    //   "image": "assets/banks/standardchartered.png",
+    //   "name": "Standard Chartered"
+    // },
+    // {
+    //   "id": '13',
+    //   "image": "assets/banks/uob.png",
+    //   "name": "United Oversea Bank"
+    // },
+    // {"id": '14', "image": "assets/banks/ocbc.png", "name": "OCBC Bank"},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final hobbytag = Padding(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.le,
+        children: <Widget>[
+          InkWell(
+            child: Image.network(
+                'https://img.icons8.com/ios-glyphs/30/000000/inspection.png'),
+            onTap: () {
+              //      Navigator.push(context,MaterialPageRoute(builder: (context){
+
+              // return new MyApp();
+
+              // }));
+            },
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            'Select Hobby',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        ],
+      ),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Hobby'),
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          hobbytag,
+         Container(
+          padding: const EdgeInsets.fromLTRB(30, 15, 30, 15),
+          decoration: BoxDecoration(
+              border: Border.all(width: 20, color: Colors.blue[200]),
+              borderRadius: BorderRadius.circular(50)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                child: DropdownButtonHideUnderline(
+                  child: ButtonTheme(
+                    alignedDropdown: true,
+                    child: DropdownButton<String>(
+                      isDense: true,
+                      hint: new Text("Select an option"),
+                      value: _selected,
+                      onChanged: (String newValue) {
+                        setState(() {
+                          _selected = newValue;
+                          _selected2 = newValue.substring(13,20);
+
+                        });
+
+                        print(_selected);
+                        print(_selected2);
+                      },
+                      items: _myJson.map((Map map) {
+                        return new DropdownMenuItem<String>(
+                          value: map["image"].toString(),
+                          // value: _mySelection,
+                          child: Row(
+                            children: <Widget>[
+                              Image.asset(
+                                map["image"],
+                                height: 120,
+                                width: 100,
+                              ),
+                              Container(
+                                  margin: EdgeInsets.only(left: 10),
+                                  child: Text(map["name"])),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height:330),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24.0),
+                  ),
+                  onPressed: () {
+                    // Navigator.push(context,MaterialPageRoute(builder: (context){
+                    //   return new EDITINFO();
+
+                    // }));
+                    // Navigator.of(context).pushNamed(HomePage.tag);
+                  },
+                  padding: EdgeInsets.all(20),
+                  color: Colors.blue,
+                  child: Text('      Cancel        ',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24.0),
+                  ),
+                  // onPressed: _saveForm(),
+                  //       Container(
+                  //   padding: EdgeInsets.all(8),
+                  //   child: RaisedButton(
+                  //     child: Text('Save'),
+                  //     onPressed: _saveForm,
+                  //   ),
+                  // ),
+                  onPressed: () {
+                    setState(() {
+                      image=_selected;
+                      name=_selected2;
+                      // _jkval = _jk;
+                      // _projectinfo = projectinfocon.text;
+                      // _assignedto = assignedtocon.text;
+                      // _description = descriptioncon.text;
+                    });
+                    // print(_myActivityResult);
+
+                    // submitProjects(_projectinfo, _description, _myActivityResult);
+                    // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    //   return new HomePage();
+                    // }));
+                  },
+
+                  // _jkval=_jk;
+
+                  // Navigator.push(context,MaterialPageRoute(builder: (context){
+                  //   return new EDITINFO();
+
+                  // }));
+                  // Navigator.of(context).pushNamed(HomePage.tag);
+
+                  padding: EdgeInsets.all(20),
+                  color: Colors.blue,
+                  child: Text('Save Changes',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                ),
+              ),
+            ],
+          ),
+          // Container(
+          //       padding: EdgeInsets.all(16),
+          //       child: Text('$image,   $name'),
+
+          //     ),
+        ],
+      ),
+    );
+  }
+}
