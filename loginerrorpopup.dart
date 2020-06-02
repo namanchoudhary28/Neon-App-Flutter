6958a7b42b39d58377432f471a1f28b9a1a51b4c
namanchoudhary28 <65090808+namanchoudhary28@users.noreply.github.com>
2020-06-02 14:34:40 +0530

    Update loginerrorpopup.dart

--- loginerrorpopup.dart
+++ loginerrorpopup.dart
@@ -1,4 +1,9 @@
-                      onPressed: (){
+           // import 'package:giffy_dialog/giffy_dialog.dart';   import this where u want to use
+// .....................add below in dependency too.............................
+            //dependencies:
+                //giffy_dialog: ^1.7.0
+//..............................................
+          onPressed: (){
                   showDialog(
                       context: context,
                       builder: (_) => NetworkGiffyDialog(
