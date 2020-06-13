a226abec053bbf73fae0de90e3b6b9cc4ffba6ff
namanchoudhary28 <65090808+namanchoudhary28@users.noreply.github.com>
2020-06-14 03:41:57 +0530

    APP BAR WITH SIDE BAR

---
+++ APP BAR WITH SIDE BAR/main_drawer.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+class MainDrawer extends StatefulWidget {
+  @override
+  _MainDrawerState createState() => _MainDrawerState();
+}
+
+class _MainDrawerState extends State<MainDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children:<Widget>[
+          Container(
+            width:double.infinity,
+            padding:EdgeInsets.only(
+              top:30,
+              bottom: 10,
+            ),
+            color: Theme.of(context).primaryColor,
+            child: Center(
+              child: Column(
+                children:<Widget>[
+                  Container(
+                    width:100,
+                    height:150,
+                    decoration:BoxDecoration(
+                      shape:BoxShape.circle,
+                      image:DecorationImage(image: NetworkImage('https://img.icons8.com/material/24/000000/phone--v1.png'),
+                      fit: BoxFit.fill
+                      ),
+                      
+
+                    )
+                  ),
+                  Text("Naman",
+                  style: TextStyle(fontSize:22,
+                  color: Colors.white),
+                  ),
+                  Text("namanc@iitbhilai.ac.in",
+                  style: TextStyle(fontSize:17,
+                  color: Colors.white),
+                  )
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Profile',
+            style: TextStyle(fontSize:16),),
+            onTap: null,
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings',
+            style: TextStyle(fontSize:16),),
+            onTap: null,
+          ),
+          ListTile(
+            leading: Icon(Icons.menu),
+            title: Text('Discover',
+            style: TextStyle(fontSize:16),),
+            onTap: null,
+          ),
+          ListTile(
+            leading: Icon(Icons.arrow_back),
+            title: Text('Logout',
+            style: TextStyle(fontSize:16),),
+            onTap: null,
+          ),
+
+        ],
+      ),
+
+    );
+  }
+}
